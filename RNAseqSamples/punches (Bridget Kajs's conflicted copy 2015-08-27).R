ec636aeee5006aa210d9b0352d24022bfe6829e3
Rayna Harris <rayna.harris@utexas.edu>
2015-08-28 10:14:50 -0500

    new bridget analysis and photos renamed

--- RNAseqSamples/punches (Bridget Kajs's conflicted copy 2015-08-27).R
+++ RNAseqSamples/punches (Bridget Kajs's conflicted copy 2015-08-27).R
@@ -21,13 +21,25 @@ barplot(counts, main="Regions per Slice",
 #make new datset with just "Mouse" "Date" "RNAisolationdate" "Slice.collector", "location"  
 #install.packages("tidyr", dependencies=TRUE)
 library("tidyr")
-library("dpdyr")
+library("dplyr")
+library("reshape2")
 
 punches %>%
-  select(Mouse, Slice, notes.mouse, notes.slice, Date, RNAisolationdate, Slice.collector) %>%
-  group_by(Mouse, notes.mouse, notes.slice, Date, Slice.collector) %>%
+  select(Mouse, Slice, notes.mouse, Date, RNAisolationdate, Slice.collector) %>%
+  group_by(Mouse, notes.mouse, Date, Slice.collector) %>%
   summarize(num.slice=max(Slice)) %>% 
   as.data.frame()->mice
-
 write.csv(mice, file="mice.csv")
-  
+
+punches %>%
+  select(Mouse, Slice, Punch, notes.mouse, Date, RNAisolationdate, Slice.collector) %>%
+  group_by(Mouse, Punch, notes.mouse, Date, Slice.collector) %>%
+  summarize(num.slice=length(Slice)) %>% 
+  as.data.frame()->CA1toDG
+
+CA1toDGWIDE<-spread(CA1toDG, Punch, num.slice)
+CA1toDGWIDE[is.na(CA1toDGWIDE)]<-0
+CA1toDGWIDE$"CA1?"<-NULL
+CA1toDGWIDE$"other"<-NULL
+CA1toDGWIDE$"Pr"<-NULL
+write.csv(CA1toDGWIDE, file="CA1toDGWIDE.csv")
