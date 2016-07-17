6ff6c88dffe65fae8c067aac1e0b781ddd977e63
raynamharris <rayna.harris@utexas.edu>
2016-07-17 16:14:13 -0400

    sample and script updates

--- bin/DataWranglingForNsbProjects.R
+++ bin/DataWranglingForNsbProjects.R
@@ -3,9 +3,11 @@
 ## All punches and tissues that Rayna has collected during the  2015 and 2016 NS&B course
 ## are described in the same csv file in BehavPhysRNAseq repo
 ## I'll use this script to create a file for the students and save it in the qPCR-mouse repo
-library(dplyr)
+
 library(plyr)
+library(dplyr)
 library(tidyr)
+library(reshape2)
 
 ## 1. Read and clean data
 ## 2. Wrangle data for students
@@ -15,67 +17,69 @@ library(tidyr)
 
 ## 1. read data
 setwd("~/Github/BehavEphyRNAseq/data/sample_info")
-punches <- read.csv("punches.csv", header=TRUE, sep="," )
-animals <- read.csv("animals.csv", header=TRUE, sep="," )
+punches <- read.csv("punches.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+animals <- read.csv("animals.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+punches$mouse <- as.factor(punches$Mouse)
+animals$mouse <- as.factor(animals$Mouse)
 str(punches)
 str(animals)
-str(punches$Mouse)
-str(animals$Mouse)
-
-## clean data 
-cleanpunches <- punches
-cleanpunches$Mouse <- as.factor(cleanpunches$Mouse)
-cleanpunches$Date <- as.Date(cleanpunches$Date, "%m/%d/%y")
-cleanpunches$year <- as.factor(cleanpunches$year)
-cleanpunches$year <- as.factor(cleanpunches$year)
-cleanpunches$RNAisolationdate <- as.Date(cleanpunches$RNAisolationdate, "%m/%d/%y")
-cleanpunches$Punchsize <- as.factor(cleanpunches$Punchsize)
-cleanpunches$Tube <- as.character(cleanpunches$Tube)
-cleanpunches$ready.time <-  strftime(cleanpunches$ready.time,"%H:%M")
-cleanpunches$year <- as.character(cleanpunches$year)
-str(cleanpunches)
 
 ## join animals and punches using the mouse id
-cleanpunches <- cleanpunches %>% 
+alldata <- punches %>% 
   left_join(animals, by="Mouse")
-  
+str(alldata)
+
+## clean data 
+alldata$Mouse <- as.factor(alldata$Mouse)
+alldata$Date <- as.Date(alldata$Date, "%m/%d/%y")
+alldata$Slice <- as.factor(alldata$Slice)
+alldata$Punch <- as.factor(alldata$Punch)
+alldata$year <- as.factor(alldata$year)
+alldata$RNAisolationdate <- as.Date(alldata$RNAisolationdate, "%m/%d/%y")
+alldata$Punchsize <- as.factor(alldata$Punchsize)
+alldata$Tube <- as.factor(alldata$Tube)
+alldata$collector <- as.factor(alldata$Tube)
+alldata$Group <- as.factor(alldata$Group)
+alldata$APA <- as.factor(alldata$APA)
+str(alldata)
+
+
 ## 3. Wrangle for students
 ## inludes Maddies Test samples
-NSBpunches <- cleanpunches %>%
-  filter(Purpose == "students", Punch != "CA1?", Mouse != "???", Mouse != "15-310", Mouse != "15-331") %>%
+NSBpunches <- alldata %>%
+  filter(Purpose == "students", Punch != "CA1?", 
+         Mouse != "???", Mouse != "15-310", Mouse != "15-331", 
+         Slice != "NA") %>%
   select(Tube, Mouse, Slice, Punch, Date, 
          ready.time, start.time, Slice.collector,
          punch.location, slice.location, storagebox, notes.x,
          Genotype, Conflict, APA, Group, collector, Purpose) %>%
   arrange(Mouse)
 str(NSBpunches)
-View(NSBpunches)
 
-## Rayna Tests
-RaynaTest <- NSBpunches %>%
-  filter(Punch != "slice", collector == "MK", Punch %in% c("18", "20", "19")) %>%
-  select(Tube, Mouse, Genotype, Group, Punch, Slice, Tube, storagebox) %>%
-  arrange(Punch, Mouse)
-View(RaynaTest)
-write.csv(RaynaTest, "RaynaTest.csv")
 
-## 4. Exercise Animals
-PracticeMice <- NSBpunches %>%
-  filter(Punch != "slice", collector == "MK") %>%
-  select(Mouse, Genotype, Group, Punch, Tube) %>%
-  arrange(Mouse)
-View(PracticeMice)
+## 4. Group Project: FMR1 - conflict - slices
+FMR1_conflict_slices <- alldata %>%
+  filter(Punch == "slice", Genotype %in% c("WT", "FMR1"), 
+         Conflict == "Conflict", Slice != "5", 
+         Mouse != "16-119C", Mouse != "16-119D", Mouse != "16-118C", Mouse != "16-118D") %>%
+  select(Mouse, Group, Slice,Punch, Tube, storagebox) %>%
+  arrange(Group, Mouse) 
+View(FMR1_conflict_slices) 
 
+FMR1_conflict_animals <- dcast(FMR1_conflict_slices, Mouse + Group ~ Slice, value.var="Slice")
+View(FMR1_conflict_animals)
 
 
 ## 5. just the huntington comparisons
-HttMice <- NSBpunches %>%
-  filter(Date < "2016-07-28", Punch != "slice", collector == "Cycle3") %>%
+HttMice <- cleanpunches %>%
+  select(Date, Mouse, Genotype, Conflict, APA, Group, Slice, Punch, collector, Tube) %>%
+  filter(Punch != "slice", collector == "Cycle3") %>%
   distinct(Mouse, Genotype, Group) %>%
   arrange(Mouse)
 
-HttGroups <- NSBpunches %>%
-  filter(Date < "2016-07-28", Punch != "slice", collector == "Cycle3") %>%
+HttGroups <- cleanpunches %>%
+  filter(Punch != "slice", collector == "Cycle3") %>%
   distinct(Mouse, Genotype, Group) %>%
   arrange(Group, Genotype) %>%
   summarise(count(Group))
